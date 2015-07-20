20ff32f820e2a0d85aa4d0c2ab36d26c969ef4b8
Krishna Patel <softeng.krishna@gmail.com>
2015-07-20 16:13:47 +0530

    Initial commit

---
+++ app/src/main/java/com/mukesh/linkedin/Config.java
@@ -0,0 +1,12 @@
+package com.mukesh.linkedin;
+
+public class Config {
+
+	public static String LINKEDIN_CONSUMER_KEY = "756j9tmzfa4wpt";
+	public static String LINKEDIN_CONSUMER_SECRET = "Y0ZoSCX93KRP3NUn";
+	public static String scopeParams = "rw_nus+r_basicprofile";
+	
+	public static String OAUTH_CALLBACK_SCHEME = "x-oauthflow-linkedin";
+	public static String OAUTH_CALLBACK_HOST = "callback";
+	public static String OAUTH_CALLBACK_URL = OAUTH_CALLBACK_SCHEME + "://" + OAUTH_CALLBACK_HOST;
+}
