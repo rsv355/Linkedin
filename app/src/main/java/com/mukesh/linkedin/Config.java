533bc299100ece959d8026354cfbe91b4875fab8
Krishna Patel <softeng.krishna@gmail.com>
2015-07-20 16:14:15 +0530

    vdsvsv

--- app/src/main/java/com/mukesh/linkedin/Config.java
+++ app/src/main/java/com/mukesh/linkedin/Config.java
@@ -2,8 +2,8 @@ package com.mukesh.linkedin;
 
 public class Config {
 
-	public static String LINKEDIN_CONSUMER_KEY = "756j9tmzfa4wpt";
-	public static String LINKEDIN_CONSUMER_SECRET = "Y0ZoSCX93KRP3NUn";
+	public static String LINKEDIN_CONSUMER_KEY = "your consumer key here";
+	public static String LINKEDIN_CONSUMER_SECRET = "your consumer secret here";
 	public static String scopeParams = "rw_nus+r_basicprofile";
 	
 	public static String OAUTH_CALLBACK_SCHEME = "x-oauthflow-linkedin";
